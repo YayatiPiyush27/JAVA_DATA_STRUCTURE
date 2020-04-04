d973635b079791af26bbab0476513f5708b64e0d
PiyushYayati <43097097+YayatiPiyush27@users.noreply.github.com>
2020-04-04 20:57:41 +0530

    Add files via upload

---
+++ BinaryTreeNodePath.java
@@ -0,0 +1,40 @@
+package yayati;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+	
+public class BinaryTreeNodePath {
+	public static ArrayList<Integer> output=new ArrayList<Integer>();
+	public static boolean nodePath(BinaryTreeNode<Integer> root,int data)
+	{
+		
+		if(root==null)
+			return false;
+		
+		if(root.data==data)
+			return true;
+		boolean left=nodePath(root.left,data);
+		boolean right=nodePath(root.right,data);
+		if(left)
+			output.add(root.left.data);
+		if(right)
+			output.add(root.right.data);
+		return left||right;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ob=new Scanner(System.in);
+		int data=ob.nextInt();
+		BinaryTreeNode<Integer> root=BinaryTreeInOutLevelWise.input();
+		boolean ans=nodePath(root,data);
+		output.add(root.data);
+		if(ans)
+			for(int i:output )
+				System.out.print(i+" ");
+		else
+			System.out.print("NOT FOUND");
+		ob.close();
+	}
+
+}
